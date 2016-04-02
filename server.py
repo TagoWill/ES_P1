86054f86ef3a5c83d21854a09f21f3e507af6312
Tiago Andrade <tagowill@gmail.com>
2016-04-02 23:10:22 +0100

    login a funcionar com BD

--- server.py
+++ server.py
@@ -1,19 +1,41 @@
 from flask import Flask, request, render_template, jsonify, abort, redirect, url_for, session
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from database import User, Base, Car, Dealership
 
 server = Flask(__name__)
+
+
+def connect_db():
+    return create_engine('mysql+pymysql://esproject:esproject@localhost:3306/esproject1')
 
 
 @server.route("/")
 def index():
     return render_template('index.html')
 
+
 @server.route("/login", methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        #verificar base de dados
-        session['logged_in'] = request.form['email']
-        return redirect(url_for('menu'))
+
+        engine = connect_db()
+        Base.metadata.bind = engine
+
+        DBSession = sessionmaker(bind=engine)
+        session2 = DBSession()
+        userexists = session2.query(User).filter_by(email=request.form['email']).first()
+        session2.close()
+        if not userexists:
+            error = 'algo nao esta bem'
+        else:
+            if userexists.email == request.form['email'] and userexists.password == request.form['password']:
+                session['logged_in'] = request.form['email']
+                return redirect(url_for('menu'))
+            else:
+                error = 'login nao correcto'
+
     return render_template('login.html', error=error)
 
 
@@ -27,6 +49,7 @@ def logout():
 def register():
     error = None
     return render_template('login.html', error=error)
+
 
 @server.route("/menu", methods=['GET'])
 def menu():
