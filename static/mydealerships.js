6d4e6bac3af818b6499c4013518768a6b625b188
Tiago Andrade <tagowill@gmail.com>
2016-04-19 19:40:08 +0100

    resolve error of link

--- static/mydealerships.js
+++ static/mydealerships.js
@@ -52,7 +52,7 @@ var MyDealerships = React.createClass({
     render: function() {
         var createItem = function(item) {
             var linha = [<td>
-                    <a href={'dealershipdetail?id='+item.id}>{item.name}</a></td>,
+                    <a href={'mydealershipdetails?id='+item.id}>{item.name}</a></td>,
                                     <td>{item.contact}</td>,<td>{item.district}</td>, <td><a href={'editdealership?id='+item.id}>click</a></td>]
             return (<tr>{linha}</tr>)
         };
